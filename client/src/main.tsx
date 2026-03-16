903514c1946a0d6bb64d2e7a796cc4e1d329af86
Nandery <siqichen0802@gmail.com>
2026-03-15 21:13:48 -0700

    Just some changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const favicons = ["/favicon.png", "/favicon2.png"];
+const randomFavicon = favicons[Math.floor(Math.random() * favicons.length)];
+const link = document.querySelector("link[rel='icon']") ?? document.createElement("link");
+link.setAttribute("rel", "icon");
+link.setAttribute("type", "image/png");
+link.setAttribute("href", randomFavicon);
+if (!document.querySelector("link[rel='icon']")) {
+  document.head.appendChild(link);
+} else {
+  (document.querySelector("link[rel='icon']") as HTMLLinkElement).href = randomFavicon;
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
